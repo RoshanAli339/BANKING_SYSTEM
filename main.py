0d601efc5c35f3bf91cba958174bf84048418624
RoshanAli339 <roshanali339lps@gmail.com>
2022-06-04 20:59:58 +0530

    Saturday 04 June 2022 08:59:58 PM IST

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ import existing
 
 def start():
     print('\t\t\tBANKING SYSTEM')
-    '''xlsx_file = '//home//xtremer//Desktop//DEXTER_BANK//accounts.xlsx'
-    wb = openpyxl.load_workbook(xlsx_file)
-    sh = wb.create_sheet(index = 0, title='sheet 1')'''
 
     print('1.Existing user\n2.New user')
     choice = int(input('Enter your choice: '))
